9dad8313fc288c9fe4a357881343db0f1482f605
kaazvaag <kaazvaag@gmail.com>
2020-05-12 16:39:05 +0200

    Create nextDNS.sh

---
+++ nextDNS.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# https://github.com/nextdns/cname-cloaking-blocklist
+
+file=/home/domains
+sudo wget https://raw.githubusercontent.com/nextdns/cname-cloaking-blocklist/master/domains -O $file
+
+while read domain
+do
+	if ! [[ "$domain" == \#* ]]; then
+		if [ ! -z "$domain" ]; then
+			regex=(\\.\|^)${domain%.*}\\.${domain##*.}$
+			sudo sqlite3 /etc/pihole/gravity.db "insert or ignore into domainlist (type, domain, enabled, comment) values (3, \"$regex\", 1, 'NextDNS CNAME list');"
+			fi
+		fi
+	done < $file
+	
+pihole restartdns reload-lists
